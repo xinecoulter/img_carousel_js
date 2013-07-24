c1c5ed0b7bd683609500265447a72e4461254374
Christine Coulter <christinetcoulter@gmail.com>
2013-07-24 19:49:56 -0400

    Create mouseover and mouseout events for next and previous buttons.

--- js/carousel.js
+++ js/carousel.js
@@ -8,5 +8,16 @@ carousel.style.marginLeft = 0;
 
 // Create mouseoveer and mouseexit events for the buttons
 // to change their opacity from .5 to .2
-
+next.onmouseover = function(event) {
+  this.style.opacity = "0.2";
+};
+next.onmouseout = function(event) {
+  this.style.opacity = "0.5";
+};
+previous.onmouseover = function(event) {
+  this.style.opacity = "0.2";
+};
+previous.onmouseout = function(event) {
+  this.style.opacity = "0.5";
+};
 // Create onclick events to advance the images
