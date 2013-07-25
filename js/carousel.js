d5f0bfd845a442fd66f339a8acbe90382d45fd38
Christine Coulter <christinetcoulter@gmail.com>
2013-07-24 21:06:47 -0400

    Create onclick events for next and previous buttons, and other supporting functions.

--- js/carousel.js
+++ js/carousel.js
@@ -4,7 +4,26 @@ var carousel = document.getElementById('carousel');
 var next = document.getElementById('next');
 var previous = document.getElementById('previous');
 
+// Carousel-related things
 carousel.style.marginLeft = 0;
+var movePixels = 612;
+var numberOfImages = document.getElementsByTagName("img").length;
+
+function carouselForward() {
+  var currentCarouselMarginLeft = parseInt(carousel.style.marginLeft);
+  carousel.style.marginLeft = (currentCarouselMarginLeft - movePixels) + "px";
+  if (Math.abs(currentCarouselMarginLeft) === ((numberOfImages - 1) * movePixels)) {
+    carousel.style.marginLeft = "0px";
+  };
+};
+
+function carouselBackward() {
+  currentCarouselMarginLeft = parseInt(carousel.style.marginLeft);
+  carousel.style.marginLeft = (currentCarouselMarginLeft + movePixels) + "px";
+  if (currentCarouselMarginLeft === 0) {
+    carousel.style.marginLeft = (-(numberOfImages - 1) * movePixels) + "px";
+  };
+};
 
 // Create mouseoveer and mouseexit events for the buttons
 // to change their opacity from .5 to .2
@@ -20,4 +39,11 @@ previous.onmouseover = function(event) {
 previous.onmouseout = function(event) {
   this.style.opacity = "0.5";
 };
+
 // Create onclick events to advance the images
+next.onclick = function(event) {
+  carouselForward();
+};
+previous.onclick = function(event) {
+  carouselBackward();
+};
